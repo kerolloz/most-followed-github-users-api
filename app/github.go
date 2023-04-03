9bbb6341c35890c9eb2f9b897426ca29bfd65a89
Kerollos Magdy <kerolloz@yahoo.com>
2023-04-03 14:45:49 +0200

    move GitHub logic to github.go

---
+++ app/github.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type User struct {
+	Name      string `json:"name"`
+	Login     string `json:"login"`
+	Bio       string `json:"bio"`
+	Followers struct {
+		TotalCount int `json:"totalCount"`
+	} `json:"followers"`
+	AvatarURL string `json:"avatarUrl"`
+}
+
+type GithubResponse struct {
+	Data struct {
+		Search struct {
+			Nodes []struct {
+				User
+			} `json:"nodes"`
+		} `json:"search"`
+	} `json:"data"`
+}
+
+func FindMostFollowedUsers(country string) GithubResponse {
+	query := fmt.Sprintf(`
+		{
+			search(query: "location:%s sort:followers-desc", type: USER, first: 50) {
+				nodes {
+					... on User {
+						name
+						login
+						bio
+						followers {
+							totalCount
+						}
+						avatarUrl
+					}
+				}
+			}
+		}
+`, country)
+
+	// Define the GraphQL request payload
+	payload := map[string]string{
+		"query": query,
+	}
+
+	// Convert the payload to a JSON string
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a new HTTP request
+	req, err := http.NewRequest("POST", "https://api.github.com/graphql", bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		panic(err)
+	}
+
+	GitHubAPIToken := os.Getenv("GITHUB_API_TOKEN")
+	// Set the authorization header to include the access token
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", GitHubAPIToken))
+
+	// Set the content type header to indicate that the request body is JSON
+	req.Header.Set("Content-Type", "application/json")
+
+	// Create a new HTTP client and send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// Parse the JSON response into a GithubResponse struct
+	var githubResp GithubResponse
+	if err := json.Unmarshal(body, &githubResp); err != nil {
+		panic(err)
+	}
+	return githubResp
+}
--- app/main.go
+++ app/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -14,38 +12,8 @@ import (
 	"github.com/joho/godotenv"
 )
 
-type User struct {
-	Name      string `json:"name"`
-	Login     string `json:"login"`
-	Bio       string `json:"bio"`
-	Followers struct {
-		TotalCount int `json:"totalCount"`
-	} `json:"followers"`
-	AvatarURL string `json:"avatarUrl"`
-}
-
-type GithubResponse struct {
-	Data struct {
-		Search struct {
-			Nodes []struct {
-				User
-			} `json:"nodes"`
-		} `json:"search"`
-	} `json:"data"`
-}
-
 func main() {
-	// Load the environment variables from the .env file
-	err := godotenv.Load("./.env")
-	isProduction := os.Getenv("PRODUCTION")
-	if err != nil && isProduction != "true" {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080" // default port number
-	}
+	loadEnvVars()
 
 	// Create a new Gorilla mux router
 	r := mux.NewRouter()
@@ -56,7 +24,7 @@ func main() {
 		country := r.URL.Query().Get("country")
 
 		// Find the top most followed users in the given country
-		githubResp := findMostFollowedUsers(country)
+		githubResp := FindMostFollowedUsers(country)
 
 		// Convert the response to a JSON string
 		jsonBytes, err := json.Marshal(githubResp.Data.Search.Nodes)
@@ -77,6 +45,11 @@ func main() {
 		handlers.AllowedOrigins([]string{"*"}),
 	)
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // default port number
+	}
+
 	fmt.Printf("Listening on :%s...\n", port)
 	if err := http.ListenAndServe(":"+port, corsMiddleware(r)); err != nil {
 		panic(err)
@@ -84,67 +57,11 @@ func main() {
 
 }
 
-func findMostFollowedUsers(country string) GithubResponse {
-	GitHubAPIToken := os.Getenv("GITHUB_API_TOKEN")
-	query := fmt.Sprintf(`
-		{
-			search(query: "location:%s sort:followers-desc", type: USER, first: 50) {
-				nodes {
-					... on User {
-						name
-						login
-						bio
-						followers {
-							totalCount
-						}
-						avatarUrl
-					}
-				}
-			}
-		}
-`, country)
-
-	// Define the GraphQL request payload
-	payload := map[string]string{
-		"query": query,
+func loadEnvVars() {
+	// Load from the .env file
+	err := godotenv.Load("./.env")
+	isProduction := os.Getenv("PRODUCTION")
+	if err != nil && isProduction != "true" {
+		log.Fatalf("Error loading .env file: %v", err)
 	}
-
-	// Convert the payload to a JSON string
-	payloadBytes, err := json.Marshal(payload)
-	if err != nil {
-		panic(err)
-	}
-
-	// Create a new HTTP request
-	req, err := http.NewRequest("POST", "https://api.github.com/graphql", bytes.NewBuffer(payloadBytes))
-	if err != nil {
-		panic(err)
-	}
-
-	// Set the authorization header to include the access token
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", GitHubAPIToken))
-
-	// Set the content type header to indicate that the request body is JSON
-	req.Header.Set("Content-Type", "application/json")
-
-	// Create a new HTTP client and send the request
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	// Read the response body
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	// Parse the JSON response into a GithubResponse struct
-	var githubResp GithubResponse
-	if err := json.Unmarshal(body, &githubResp); err != nil {
-		panic(err)
-	}
-	return githubResp
 }
